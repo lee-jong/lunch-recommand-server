d672b37119906339f9be9ea0e4022bcea5a32956
Bell <jay@clobot.co.kr>
2024-07-04 15:13:58 +0900

    [v1.0.1] schedule 평일에만 발송되도록 수정

--- lib/schedule.ts
+++ lib/schedule.ts
@@ -1,6 +1,6 @@
 import schedule from "node-schedule";
 import { sendRecommand } from "controller/recommand";
 
-schedule.scheduleJob("0 0 14 * * *", async () => {
+schedule.scheduleJob("0 20 11 * * 1-5", async () => {
   sendRecommand();
 });
